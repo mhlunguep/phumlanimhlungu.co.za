c7c1091d3903955b962537ee6bbf9f0c2a7c4899
Phumlani Mhlungu <74601957+mhlunguep@users.noreply.github.com>
2021-07-24 11:38:48 +0200

    Add Index File

---
+++ index.php
@@ -0,0 +1,335 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+       <?php include 'includes/head.php';?>
+
+    </head>
+    <body id="page-top">
+       <?php include 'includes/navbar.php';?>
+
+        <div class="container-fluid p-0">
+            <!-- About-->
+            <section class="resume-section" id="about">
+                <div class="resume-section-content">
+                    <h3>Hello, I'm</h3> 
+                    
+                    <h1 class="mb-0">
+                        Phumlani
+                        <span class="text-primary">Mhlungu</span>
+                    </h1>
+                    <div class="subheading mb-5">
+                        Based in Durban, can work remotely, and willing to relocate.
+                    </div>
+                    <h3>A Full-stack Developer</h3>
+                    <p class="lead mb-5">With over 3 years experience in HTML5, CSS3, JavaScript 6, jQuery, MySQL and PHP. 
+                        I have developed, launched and maintained multiple projects from scratch, carrying the development of it's front-end and back-end code bases.</p>
+                        
+                    <a href="assets\img\my-cv.pdf" download="my-cv.pdf"><button type="button" class="btn btn-primary"><i class="fa fa-download mr-2"></i>Download CV</button></a>
+                    <a href="#contact"><button type="button" class="btn btn-outline-primary">Contact Me</button><a/>
+
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Experience-->
+            <section class="resume-section" id="experience">
+                <div class="resume-section-content">
+                    <h2>Experience</h2>
+                    <p>What I can do for you</p>
+                    <hr width="5%" noshade="5" align="left">
+                    
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Full-Stack Developer</h3>
+                            <div class="subheading mb-3">Multimedia Tech Design</div>
+                            <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring. My tasks included but not limited to:</p>
+                            <ul>
+                                <li>Build Client websites using WordPress or Bootstrap</li>
+                                <li>Customize themes or plugins using HTML, CSS, JavaScipt, PHP, MySQL, and
+                                    JQuery to meet clients’ requirements.
+                                </li>
+                                <li>Meet with clients to discuss website design and function.</li>
+                                <li>Work with stakeholders across the company to ensure tasks and projects are executed to satisfaction.</li>
+                                <li>Conducting website performance tests.</li>
+                                <li>Maintain and update WP instance, plugins, websites, and manage security requirements/specifications</li>
+                                <li>Adapting existing theme frameworks to create a unique client experience</li>
+                                <li>Maintain running websites</li>
+                                <li>Install premium themes and child themes</li>
+                                <li>Create or move emails</li>
+                                <li>Backup websites, databases, emails</li>
+                                <li>Move website from one domain or hosting to another</li>
+                                <li>Perform troubleshooting after launching and while hosting a website</li>
+                                <li>Provide website training with the client</li>
+                            </ul>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">January 2018 - June 2021</span></div>
+                    </div>
+                    
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Education-->
+
+            <section class="resume-section" id="education">
+                <div class="resume-section-content">
+                    <h2>Education</h2>
+                    <p>Where I got my qualifications</p>
+                    <hr width="5%" noshade="5" align="left">
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <img src="assets/img/ukzn.png" class="pb-3">
+                            <div class="subheading mb-3">Bachelor of Science</div>
+                            <div>IT and Statistics</div>                
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">Graduated 2013</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between">
+                        <div class="flex-grow-1">
+                            <img src="assets/img/ukzn.png" class="pb-3">
+                            <div class="subheading mb-3">Bachelor of Commerce Honours</div>
+                            <p>Information Systems and Technology</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">Graduated 2014</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+
+            <!-- Skills-->
+            <section class="resume-section" id="skills">
+                <div class="resume-section-content">
+                    
+                    <h2>Skills</h2>
+                    <p>What I'm good at</p>
+                    <hr width="5%" noshade="5" align="left">
+                    <div class="subheading my-3">Programming Languages & Tools</div>
+                    
+                    <ul class="list-inline dev-icons">
+                        <li class="list-inline-item"><i class="fab fa-html5"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-wordpress"></i></li>
+                        <img src="assets/img/woocommerce.png" width="64" height="64">
+                        <li class="list-inline-item"><i class="fab fa-php"></i></li>
+                        <img src="assets/img/mysql.png" width="50" height="50">
+                        <li class="list-inline-item"><i class="fab fa-git"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-github"></i></li>
+                        <img src="assets/img/photoshop.png" width="42" height="42">
+                        <img src="assets/img/visual-studio.png" width="50" height="50">
+                        
+                    
+                    </ul>
+
+                    <div class="subheading mb-3">Workflow</div>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Mobile-First, Responsive Design
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Cross Browser Testing & Debugging
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Cross Functional Teams
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Agile Development & Scrum
+                        </li>
+                    </ul>
+
+                    <div class="subheading my-3">Soft Skills</div>
+                    <ul>
+                        <li>I am <b>a researcher</b>, I like to keep myself updated with  market trends and industry requirements.</li>
+                        <li>I am <b>born leader</b>, wherever I am I inspire others and encourage them to achieve their full potential. </li>
+                        <li>Call me <b>a strategic thinker</b> when it comes to problem solving.</li>
+                    </ul>
+                </div>
+            </section>
+            <hr class="m-0" />
+
+            <section class="resume-section" id="portfolio">
+                <div class="resume-section-content">
+                    
+                    <h2>Portfolio </h2>
+                    <p>A taste of what I have done</p>
+                    <hr width="5%" noshade="5" align="left">
+                    <div class="subheading my-3">See some of my works below. Unless explicitly stated otherwise, 
+                        all their front-end and back-end parts were completely done by me.
+                    </div>
+                <!--Portfolio Grid-->
+
+                <div class="container">
+            <div class="grid">
+
+                <div class="row">
+                    <div class="col">                    
+                        <figure class="effect-ravi">
+                            <a href="juicebros.php">
+                            <img src="assets/img/961-1000x805.jpg" alt="JuiceBros" width="" height=""/>
+                            
+                            <figcaption>
+                                <h2>Juice<span>Bros</span></h2>
+                            </figcaption>
+                            </a>
+                        </figure>
+                    </div>
+                    <div class="col">
+                        <figure class="effect-ravi">
+                            <a href="realreflections.php">
+                                <img src="https://picsum.photos/1000/805/?random" alt="img25" />
+                            <figcaption>
+                                <h2>Real Reflections <span>Photography</span></h2>
+                            </figcaption>
+                            </a>
+                        </figure>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <figure class="effect-ravi">
+                            <a href="urbanelife.php">
+                                <img src="https://picsum.photos/1000/800/?random" alt="img17" />
+                                <figcaption>
+                                    <h2>Urban<span>elife</span></h2>
+                                </figcaption>
+                            </a>
+                        </figure>
+                    </div>
+                    <div class="col-md-4">
+                        <figure class="effect-ravi">
+                            <a href="umthombo.php">
+                                <img src="https://picsum.photos/1000/801/?random" alt="img25" />
+                                <figcaption>
+                                    <h2>Umthombo Womusa <span>Funeral Services</span></h2>
+                                </figcaption>
+                            </a>
+                        </figure>
+                    </div>
+                    <div class="col-md-4">
+                        <figure class="effect-ravi">
+                            <a href="multimedia.php">
+                                <img src="https://picsum.photos/1000/802/?random" alt="img25" />
+                                <figcaption>
+                                    <h2>Multimedia <span>Tech  Design</span></h2>
+                                </figcaption>
+                            </a>
+                        </figure>
+                    </div>
+                </div>
+                
+            </div>
+
+        </div>
+
+                    
+        
+            </section>
+            
+            <hr class="m-0" />
+            <!-- Interests-->
+            <section class="resume-section" id="interests">
+                <div class="resume-section-content">
+                    <h2>Interests</h2>
+                    <p>Apart from being a developer</p>
+                    <hr width="5%" noshade="5" align="left">
+                    <p>I enjoy most of my time being outdoors with my wife and our daughter. In the winter, 
+                        we visit our families and enjoy braais. During the warmer months here in Durban, 
+                        we enjoy swimming at the beach, ridding biycles, and going out on a picnic park.
+                    </p>
+                    <p class="mb-0">When forced indoors, I follow a number of commedy genre movies 
+                        and television shows, I am an aspiring handyman, and I spend a large amount of my free time exploring 
+                        the latest technology advancements in the software development world.
+                    </p>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Contact-->
+            <section class="resume-section" id="contact">
+                <div class="resume-section-content">
+                    <h2>Contact Me</h2>
+                    <p>How you can get connnected with me</p>
+                    <hr width="5%" noshade="5" align="left">
+                 
+                    <div class="social-icons col-12">
+                        <a class="social-icon" href="https://www.linkedin.com/in/phumlani-mhlungu-760b2856/" target=_blank><i class="fab fa-linkedin-in"></i></a>
+                        <a class="social-icon" href="https://github.com/mhlunguep" target=_blank><i class="fab fa-github"></i></a>
+                        <a class="social-icon" href="https://twitter.com/epmhlungu" target=_blank><i class="fab fa-twitter"></i></a>
+                        <a class="social-icon" href="https://facebook.com/mhlunguep" target=_blank><i class="fab fa-facebook-f"></i></a>
+                    </div>
+                 
+                    <div class="subheading my-3">I am available for hire and open to any ideas of cooperation. Please fill in the following form. I respond within 24 hours.</div>
+                    <!--Contact Form-->
+                    <form id="contact-form" class="py-4" method="post" action="contact.php" role="form">
+
+                        <div class="messages"></div>
+                    
+                        <div class="controls">
+                    
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="form_name">First name *</label>
+                                        <input id="form_name" type="text" name="name" class="form-control" placeholder="Enter your first name *" required="required" data-error="Firstname is required.">
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="form_lastname">Email *</label>
+                                        <input id="form_email" type="email" name="email" class="form-control" placeholder="Enter your email *" required="required" data-error="Valid email is required.">
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="form_email">Phone *</label>
+                                        <input id="form_phone" type="tel" name="phone" class="form-control" placeholder="Enter your phone number *" required="required" data-error="Valid phone number is required.">
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="form_need">Company </label>
+                                        <input id="form_phone" type="text" name="company" class="form-control" placeholder="Enter your company name">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="form_message">Message *</label>
+                                        <textarea id="form_message" name="message" class="form-control" placeholder="Message for me *" rows="4" required="required" data-error="Please, leave us a message."></textarea>
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <input type="submit" class="btn btn-primary btn-send" value="Send message">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <p class="text-muted">
+                                        * Fields are required.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    
+                    </form>
+                    
+                </div>              
+            </section>
+        </div>
+
+        <!-- Footer-->
+        <footer>
+            <?php include 'includes/footer.php';?>
+        </footer>
+
+    </body>
+</html>
