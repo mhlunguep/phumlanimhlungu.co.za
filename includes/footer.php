7741ff305268b342fe4e3286da5264fa3567f8b4
Phumlani Mhlungu <74601957+mhlunguep@users.noreply.github.com>
2021-07-24 11:15:27 +0200

    Updated my portfolio website
    This is an updated version of my wesite

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+<div class="text-center">
+    <p>
+        <i class="fa fa-copyright mr-1" aria-hidden="true"></i>
+    Copyright <?php echo date('Y'); ?>. All right reserved.
+    </p>               
+</div>
+
+<!-- Bootstrap core JS-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Third party plugin JS-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+<!-- Core theme JS-->
+<script src="js/scripts.js"></script>
+<script src="/js/contact.js"></script>
+
+<script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+
+
---
+++ includes/portfolio.php
@@ -0,0 +1,39 @@
+<div class="container">
+    <div class="grid">
+        <div class="row">
+            <div class="col-md-4">
+                <figure class="effect-ravi">
+                    <a href="urbanelife.php">
+                        <img src="https://picsum.photos/1000/800/?random" alt="img17" />
+                        <figcaption>
+                            <h2>Urban<span>elife</span></h2>
+                        </figcaption>
+                    </a>
+                </figure>
+            </div>
+                    
+            <div class="col-md-4">
+                <figure class="effect-ravi">
+                    <a href="umthombo.php">
+                        <img src="https://picsum.photos/1000/801/?random" alt="img25" />
+                        <figcaption>
+                             <h2>Umthombo Womusa <span>Funeral Services</span></h2>
+                        </figcaption>
+                    </a>
+                </figure>
+            </div>
+                    <div class="col-md-4">
+                        <figure class="effect-ravi">
+                            <a href="multimedia.php">
+                                <img src="https://picsum.photos/1000/802/?random" alt="img25" />
+                                <figcaption>
+                                    <h2>Multimedia <span>Tech  Design</span></h2>
+                                </figcaption>
+                            </a>
+                        </figure>
+                    </div>
+                </div>
+                
+            </div>
+
+        </div>
